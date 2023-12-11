448e02c3f7e56501eb6cf4b909d06a7a987d888b
shrimptrimp <kasperkukkonen08@gmail.com>
2023-12-11 13:07:03 +0200

    organized more stuff

---
+++ scripts/RigidBody2D.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+
+@export var speed = 400
+@export var velocity = 400
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
